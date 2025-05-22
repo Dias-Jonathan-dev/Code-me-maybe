1327701ac57d9dd45585aee8ebc2860efd5f91d4
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-05-22 14:55:11 +0200

    add HomePage and EndPage

---
+++ src/Components/HomePage.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
+function HomePage() {
+	const navigate = useNavigate();
+	const handleClick = () => {
+		navigate("/game");
+	};
+
+	return (
+		<div className="homepage">
+			<h1>Code Me Maybe !</h1>
+			<p>
+				Bienvenue sur <span>Code Me Maybe.</span>
+				<br />
+				Apprend les bases du langage CSS en t'amusant !
+			</p>
+			<button type="button" onClick={handleClick}>
+				Start
+			</button>
+		</div>
+	);
+}
+
+export default HomePage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import HomePage from "./Components/HomePage.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import EndPage from "./Components/EndPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "/game",
+		element: <App />,
+	},
+	{
+		path: "/end",
+		element: <EndPage />,
+	},
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
+
+createRoot(rootElement).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
